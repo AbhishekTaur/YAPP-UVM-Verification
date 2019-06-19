ed71f1a34fa9d560dac8d0c11d62bacdca44992e
Abhishek Taur <abhishektaur123@gmail.com>
2019-06-19 16:55:45 -0500

    Removed unnecessary files

--- uvm_training_1.1/uvm/lab09c/tb/INCA_libs/irun.lnx86.15.20.nc/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1382987775 /softwares/Linux/cadence/INCISIVE152/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1415723548 /softwares/Linux/cadence/INCISIVE152/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
